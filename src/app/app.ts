d547ac954887ca0eaddc05bfee40444561bfbe12
ofmarin <ofelmaran@gmail.com>
2026-05-13 20:09:47 -0400

    html structure distributed among components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Description} from './description/description';
+import {PrepTime} from './prep-time/prep-time';
+import {Instructions} from './instructions/instructions';
+import {Nutrition} from './nutrition/nutrition';
+import {Ingredients} from './ingredients/ingredients';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Description, PrepTime, Nutrition, Instructions, Ingredients],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrls: ['./app.css'],
 })
 export class App {
-  omelette = "./images/image-omelette.jpeg"
+  omelette = './images/image-omelette.jpeg';
   protected readonly title = signal('recipe-page');
 }
